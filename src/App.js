7262a9a2108c539b7ecf8805ad9488cbebbe6f97
Rain <rkullamaa@gmail.com>
2020-05-15 11:15:41 +0200

    testing

--- src/App.js
+++ src/App.js
@@ -42,7 +42,6 @@ const App = () => {
           <PrivateRoute exact path='/scan' component={Scan} /> 
           <Route exact path="/login" component={Login} />
           <Route exact path="/signup" component={SignUp} />
-          
       </Router>
     </AuthProvider>
   );
